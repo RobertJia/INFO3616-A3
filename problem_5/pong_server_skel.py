aa55837320a647b2989a3e6e8e2fb39a51fe26c2
RobertJia <sjia0385@uni.sydney.edu.au>
2020-11-07 03:01:46 +1100

    Q5 done

--- problem_5/pong_server_skel.py
+++ problem_5/pong_server_skel.py
@@ -9,20 +9,26 @@ port = 4433
 def handle(conn):
     while True:
         # YOUR TASK STARTS HERE
-        ...
-
+        res = conn.recv().decode().strip()
+        if (res=='PING'):
+            conn.send(bytes('PONG\n', 'utf-8'))
+    
 
 
 def main():
     # Create a socket and correct context (3cr)
-    # ...
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain('server_cert.pem', 'server_key.pem')
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+    sock.bind((hostname, port))
+    sock.listen(5)
     print("Listening.")
     while True:
         conn = None
-        ssock, addr = sock.accept()
         try:
             # Final step to use the context ... (1cr)
-            # ...
+            ssock = context.wrap_socket(sock, server_side=True)
+            conn, addr = ssock.accept()
             handle(conn)
         except Exception as e:
             print(e)
